9b27d15764ff9a446515e5b4310c975b4d513f71
HariHar-an-and <hariharan.anand@anywhere.co>
2020-03-06 19:35:22 +0530

    formatted to aw - done by user

--- src/SampleFile.java
+++ src/SampleFile.java
@@ -1,13 +1,13 @@
 
 public class SampleFile {
 
-	public static void main(String[] args) {
-		System.out.println("eclipse style with some changes");
-		int x[] = {1, 2, 3, 4, 5, 6, 7, 8};
-		for (int n : x) {
-			System.out.println(n);
-		}
-		System.out.println("numbers");
-	}
+  public static void main(String[] args) {
+    System.out.println("eclipse style with some changes");
+    int x[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    for (int n : x) {
+      System.out.println(n);
+    }
+    System.out.println("numbers");
+  }
 
 }
