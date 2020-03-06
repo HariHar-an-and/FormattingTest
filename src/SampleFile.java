366702916212ea50260966215776f29c278026e0
HariHar-an-and <hariharan.anand@anywhere.co>
2020-03-06 19:32:59 +0530

    formatted by aw - gitbot

--- src/SampleFile.java
+++ src/SampleFile.java
@@ -1,13 +1,13 @@
 
 public class SampleFile {
 
-	public static void main(String[] args) {
-		System.out.println("eclipse style");
-		int x[] = {1, 2, 3, 4, 5};
-		for (int n : x) {
-			System.out.println(n);
-		}
-		System.out.println("numbers");
-	}
+  public static void main(String[] args) {
+    System.out.println("eclipse style");
+    int x[] = {1, 2, 3, 4, 5};
+    for (int n : x) {
+      System.out.println(n);
+    }
+    System.out.println("numbers");
+  }
 
 }
