b343b5a818c0815d488d14fe88f041640cd0eb0e
HariHar-an-and <hariharan.anand@anywhere.co>
2020-03-06 18:59:01 +0530

    awstyle

--- src/SampleFile.java
+++ src/SampleFile.java
@@ -3,9 +3,9 @@ public class SampleFile {
 
   public static void main(String[] args) {
     System.out.println("eclipse style");
-    int x[] = {1,2,3,4,5};
-    for(int n : x) {
-    	System.out.println(n);
+    int x[] = {1, 2, 3, 4, 5};
+    for (int n : x) {
+      System.out.println(n);
     }
     System.out.println("numbers");
   }
