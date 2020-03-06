8b8d747693d288e9b90a47123cb23fe1b8daa6a7
HariHar-an-and <hariharan.anand@anywhere.co>
2020-03-06 19:32:05 +0530

    with some changes

--- src/SampleFile.java
+++ src/SampleFile.java
@@ -2,8 +2,8 @@
 public class SampleFile {
 
 	public static void main(String[] args) {
-		System.out.println("eclipse style");
-		int x[] = {1, 2, 3, 4, 5};
+		System.out.println("eclipse style with some changes");
+		int x[] = {1, 2, 3, 4, 5, 6, 7, 8};
 		for (int n : x) {
 			System.out.println(n);
 		}
